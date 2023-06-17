350507fbd61758da6a68c75f062b6f9fb777c8ec
bero <daiki.guitar.1122@gmail.com>
2023-06-17 22:24:03 +0900

    お試し実装

--- monkeyBanana/Assets/Scripts/click.cs
+++ monkeyBanana/Assets/Scripts/click.cs
@@ -1,29 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class click : MonoBehaviour
 {
     public GameObject tamagokakegohan;
     public GameObject siroigohan;
+
+    [SerializeField] private double minSecondsForChengetexture;
+    [SerializeField] private double maxSecondsForChengetexture;
+    [SerializeField] private Text _timerText;
+
+    private Vector2 BeforePosTama;
+    private Vector2 BeforePosSiro;
+
+    private bool isTimer = false;
+    private float TimerCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(waitSecond(GetRndSeconds()));
+        Transform tamagokake = tamagokakegohan.transform;
+        Transform sirogohan = siroigohan.transform;
+        BeforePosTama = tamagokake.position;
+        BeforePosSiro = sirogohan.position;
+        sirogohan.position = BeforePosTama;
+        tamagokake.position = BeforePosSiro;
+
+        if (BeforePosSiro.Equals(tamagokakegohan.transform.position))
+        {
+            isTimer = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isTimer)
+        {
+            TimerCount += Time.deltaTime;
+        }
         if (Input.GetMouseButtonDown(0))
         {
-            Transform tamagokake = tamagokakegohan.transform;
-            Transform sirogohan = siroigohan.transform;
-
-            Vector2 postama = tamagokake.position;
-            Vector2 possiro = sirogohan.position;
-            sirogohan.position = postama;
-            tamagokake.position = possiro;
+            isTimer = false;
+            _timerText.text = TimerCount.ToString("f3");
         }
     }
+
+    IEnumerator waitSecond(float f)
+    {
+        yield return new WaitForSeconds(f);
+    }
+
+    private float GetRndSeconds()
+    {
+        System.Random rnd = new System.Random();
+        //ê›íËÇµÇΩç≈ëÂÅAç≈è¨ÇÃÇ»Ç©Ç≈ÉâÉìÉ_ÉÄÇ≈ïbêîÇåàíË
+        double rndSeco = rnd.NextDouble() * (maxSecondsForChengetexture - minSecondsForChengetexture) + minSecondsForChengetexture;
+
+        return (float)rndSeco;
+    }
+    //private void FixedUpdate()
+    //{
+    //    if (isTimer)
+    //    {
+    //        TimerCount += Time.deltaTime;
+    //    }
+    //    _timerText.text = TimerCount.ToString("f3");
+    //}
 }
